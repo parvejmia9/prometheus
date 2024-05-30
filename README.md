6132576011e6f01fdeeb9334697ec8b202f2e12a
parvejmia9 <parvejmia9@gmail.com>
2024-05-30 15:28:50 +0600

    final touch

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # prometheus
+
+```shell
+go run main.go start -p 8181
+localhost:8181/ping
+localhost:8181/metrics
+```
+Run this command to execute 10 pings to the server. Then check the counter
+increases on the metrics url. 
+```shell
+./ping_test.sh
+```
--- ping_test.sh
+++ ping_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Loop to run the curl command 10 times to test the counter is increasing
-for i in {1..100}
+for i in {1..10}
 do
   # Print the command to the terminal
   echo "$i. Executing command: curl localhost:8081/ping"
 
   # Execute the curl command
-  curl muktoacademy.com
+  curl localhost:8081/ping
 done
